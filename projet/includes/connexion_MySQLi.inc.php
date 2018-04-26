71611d27731257b9341a9ee3b407cafddd2bd0b8
Sivdan777 <31362154+Sivdan777@users.noreply.github.com>
2018-04-26 13:59:33 -0400

    Add files via upload

---
+++ projet/includes/connexion_MySQLi.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbServername = "localhost";
+$dbUserername = "root";
+$dbPassword = "";
+$dbName = "sportedin";
+
+$connexion = mysqli_connect($dbServername,$dbUserername,$dbPassword,$dbName);
+
+ ?>
