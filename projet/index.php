606eae056af2c14f0c07b296de81d0121c7306f9
Samuel <ld.samuel.houle@gmail.com>
2018-05-01 12:27:18 -0400

    preparation pour ajax

--- projet/index.php
+++ projet/index.php
@@ -63,9 +63,7 @@ echo "</pre>";
       <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
     </div>
   </nav>
-    
-	</form>
-  
+  <div class="content">
     <div class="container" id="banniere">
       <h1 class="header center orange-text">Réseau sportif</h1>
       <div class="row center">
@@ -268,62 +266,63 @@ echo "</pre>";
 		  </div>
 		 </div>
 		</div>
+		<div class="temoignage">
+			<div class="row">
+				<div class="col s12 m3">
+				  <div class="card">
+					<div class="card-image">
+					  <img src="http://www.mstrafo.de/fileadmin/_processed_/b/1/csm_person-placeholder-male_5602d73d5e.png">
+					  <span class="card-title">Card Title</span>
+					</div>
+					<div class="card-content">
+					  <p>I am a very simple card. I am good at containing small bits of information.
+					  I am convenient because I require little markup to use effectively.</p>
+					</div>
+				  </div>
+				</div>
 		
-		<div class="row">
-			<div class="col s12 m3">
-			  <div class="card">
-				<div class="card-image">
-				  <img src="http://www.mstrafo.de/fileadmin/_processed_/b/1/csm_person-placeholder-male_5602d73d5e.png">
-				  <span class="card-title">Card Title</span>
+				<div class="col s12 m3">
+				  <div class="card">
+					<div class="card-image">
+					  <img src="http://www.mstrafo.de/fileadmin/_processed_/b/1/csm_person-placeholder-male_5602d73d5e.png">
+					  <span class="card-title">Card Title</span>
+					</div>
+					<div class="card-content">
+					  <p>I am a very simple card. I am good at containing small bits of information.
+					  I am convenient because I require little markup to use effectively.</p>
+					</div>
+				  </div>
 				</div>
-				<div class="card-content">
-				  <p>I am a very simple card. I am good at containing small bits of information.
-				  I am convenient because I require little markup to use effectively.</p>
+			
+				<div class="col s12 m3">
+				  <div class="card">
+					<div class="card-image">
+					  <img src="http://www.mstrafo.de/fileadmin/_processed_/b/1/csm_person-placeholder-male_5602d73d5e.png">
+					  <span class="card-title">Card Title</span>
+					</div>
+					<div class="card-content">
+					  <p>I am a very simple card. I am good at containing small bits of information.
+					  I am convenient because I require little markup to use effectively.</p>
+					</div>
+				  </div>
 				</div>
-			  </div>
-			</div>
-	
-			<div class="col s12 m3">
-			  <div class="card">
-				<div class="card-image">
-				  <img src="http://www.mstrafo.de/fileadmin/_processed_/b/1/csm_person-placeholder-male_5602d73d5e.png">
-				  <span class="card-title">Card Title</span>
+			
+				<div class="col s12 m3">
+				  <div class="card">
+					<div class="card-image">
+					  <img src="http://www.mstrafo.de/fileadmin/_processed_/b/1/csm_person-placeholder-male_5602d73d5e.png">
+					  <span class="card-title">Card Title</span>
+					</div>
+					<div class="card-content">
+					  <p>I am a very simple card. I am good at containing small bits of information.
+					  I am convenient because I require little markup to use effectively.</p>
+					</div>
+				  </div>
 				</div>
-				<div class="card-content">
-				  <p>I am a very simple card. I am good at containing small bits of information.
-				  I am convenient because I require little markup to use effectively.</p>
-				</div>
-			  </div>
-			</div>
-		
-			<div class="col s12 m3">
-			  <div class="card">
-				<div class="card-image">
-				  <img src="http://www.mstrafo.de/fileadmin/_processed_/b/1/csm_person-placeholder-male_5602d73d5e.png">
-				  <span class="card-title">Card Title</span>
-				</div>
-				<div class="card-content">
-				  <p>I am a very simple card. I am good at containing small bits of information.
-				  I am convenient because I require little markup to use effectively.</p>
-				</div>
-			  </div>
-			</div>
-		
-			<div class="col s12 m3">
-			  <div class="card">
-				<div class="card-image">
-				  <img src="http://www.mstrafo.de/fileadmin/_processed_/b/1/csm_person-placeholder-male_5602d73d5e.png">
-				  <span class="card-title">Card Title</span>
-				</div>
-				<div class="card-content">
-				  <p>I am a very simple card. I am good at containing small bits of information.
-				  I am convenient because I require little markup to use effectively.</p>
-				</div>
-			  </div>
 			</div>
 		</div>
-	</div>	 
-
+	</div>
+	</div>
   <footer class="page-footer green">
     
     <div class="footer-copyright">
